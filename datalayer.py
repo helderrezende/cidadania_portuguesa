7683d145d8685cacf86438d23ee7189ba5a5a1e9
Helder Rezende <helder@carteiraglobal.com>
2020-06-28 00:12:43 -0300

    feat: manual data

--- datalayer.py
+++ datalayer.py
@@ -1,15 +1,19 @@
 import pandas as pd
 
-PATH_EXCEL = "Arquivo Central.xlsx"
+PATH_EXCEL = "data/Arquivo Central.xlsx"
+PATH_EXCEL_MANUAL = "data/Cidadania_manual.xlsx"
 
-def get_acp_data(path):
+def get_acp_data():
     COLUMNS_EXCEL = ['numero_processo', 'year', 
                      'Dt. Recepção (pelo-AR) sab/dom',
                      'Despacho de Aprovação sab/dom',
                      'Data de Conclusão (Assento) sab/dom',
                      'Status', 'Exig & Urg']
     
-    acp_excel = pd.read_excel(path)
+    acp_excel = pd.read_excel(PATH_EXCEL)
+    manual_excel = pd.read_excel(PATH_EXCEL_MANUAL)
+
+    acp_excel = pd.concat([acp_excel, manual_excel], sort=False)
     acp_excel = acp_excel.dropna(subset=["Número do processo"])
     acp_excel['numero_processo'], acp_excel['year'] = acp_excel["Número do processo"].str.split("/").str
     
